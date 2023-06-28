44a70b6b89eb08fc38e59878265eee6f7bd8e660
Akshay Rathod <kr.akshay234@gmail.com>
2023-06-28 16:02:19 +0530

    sortList updated

--- controllers/products.js
+++ controllers/products.js
@@ -2,14 +2,12 @@ const Product = require('../models/product');
 
 const getAllProductsStatic = async (req, res) => {
     const search = 'em';
-    const products = await Product.find({
-        name: { $regex: search, $options: 'i' },
-    })
+    const products = await Product.find({}).sort('-name price').select('name price');
     res.status(200).json({ products, nbHits: products.length });
 }
 
 const getAllProducts = async (req, res) => {
-    const { featured, company, name } = req.query;
+    const { featured, company, name, sort, fields } = req.query;
     const queryObject = {};
     if (featured) {
         queryObject.featured = featured === 'true' ? true : false;
@@ -20,8 +18,20 @@ const getAllProducts = async (req, res) => {
     if (name) {
         queryObject.name = { $regex: name, $options: 'i' };
     }
-    console.log(queryObject);
-    const products = await Product.find(queryObject);
+    // console.log(queryObject);
+    let result = Product.find(queryObject);
+    if (sort) {
+        sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    }
+    else {
+        result = result.sort('createdAt');
+    }
+    if(fields) {
+        
+    }
+
+    const products = await result;
     res.status(200).json({ products, nbHits: products.length });
 }
 
